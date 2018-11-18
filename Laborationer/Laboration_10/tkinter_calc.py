400b3b64ecfab7c5c9c2cc95a7777d2a9b1c7be5
Thomas Andersson <thoan741@gmail.com>
2018-11-18 18:54:27 +0100

    Ett till commit

--- Laborationer/Laboration_10/tkinter_calc.py
+++ Laborationer/Laboration_10/tkinter_calc.py
@@ -1,6 +1,6 @@
 from tkinter import *
-import calcQueue
-import calcStack
+from calcCommand import *
+
 
 class tkCalc():
 
@@ -8,10 +8,12 @@ class tkCalc():
         self.root = Tk()
         self.var = StringVar()
         self.var.set("")
+        self.ans = StringVar()
+        self.ans.set("")
 
     def windowTopLevel(self,xSize, ySize):
         self.root.overrideredirect(1)
-        self.root.geometry(str(xSize) +'x' +str(ySize))
+        self.root.geometry(str(xSize) + 'x' +str(ySize))
         self.title = Frame(self.root, bg='lightBlue', relief='raised', bd=2)
         self.close = Button(self.title, text='X', command=self.root.destroy)
         self.text = Label(self.title, text="En fin miniräknare", bg="lightblue")
@@ -25,49 +27,49 @@ class tkCalc():
     def move_window(self, event):
         self.root.geometry('+{0}+{1}'.format(event.x_root, event.y_root))
 
-    def calcInput(self):
+    def checkInput(self):
         try:
-            variable = int(self.var.get())
-            print(variable)
+            Var = float(self.var.get())
+            return True
         except ValueError:
-            print("ej heltal")
-        self.var.set("")
+            print("Tal måste matas in!")
+            return False
 
-    def push(self):
-        try:
-            variable = int(self.var.get())
-            print(variable)
-        except ValueError:
-            print("ej heltal")
-        self.var.set("")
+    def buttonPress(self, text):
+        if text == "Push":
+            if self.checkInput():
+                S.push(int(self.var.get()))
+            self.var.set("")
 
-    def add(self):
-        print("Wow, plus, kanske ska implementera detta")
+        elif text == "+":
+            print("Wow, plus, kanske ska implementera detta")
+            Q.enqueue(text)
+        elif text == "-":
+            print("Wow, minus, kanske ska implementera detta")
+        elif text == "*":
+            print("Wow, gånger, kanske ska implementera detta")
+        elif text == "/":
+            print("Wow, division, kanske ska implementera detta")
+        elif text == "^":
+            print("Wow, upphöjt till, kanske ska implementera detta")
+        elif text == "=":
+            print("Wow, likhet, kanske ska implementera detta")
+            var1 = S.pop()
+            var2 = S.pop()
+            op = Q.dequeue()
+            if op == "+":
+                Var = var2 + var1
+            S.push(Var)
+            self.ans.set(str(Var))
 
-    def sub(self):
-        print("Wow, minus, kanske ska implementera detta")
-
-    def mul(self):
-        print("Wow, gånger, kanske ska implementera detta")
-
-    def div(self):
-        print("Wow, division, kanske ska implementera detta")
-
-    def eq(self):
-        print("Wow, likhet, kanske ska implementera detta")
-
-    def pwr(self):
-        print("Wow, upphöjt till, kanske ska implementera detta")
-
-    def exit(self):
-        quit("Då packar vi ihop för denna gången, ha det bra!")
-
-    def printHelp(self):
-        print("""Skriv in tal i inmatningsfältet. 
-        Knappen push pushar då talet till stacken.
-        Operatorerna pushas till kön, likhetstecken tar ut två operander ur stacken och en
-        operator ur kön och applicerar operatorn mellan dessa för att sedan pusha svaret tillbaka till stacken.
-        Exit knappen avslutar programmet.""")
+        elif text == "EXIT":
+            quit("Då packar vi ihop för denna gången, ha det bra!")
+        elif text == "Help":
+            print("""Skriv in tal i inmatningsfältet. 
+                    Knappen push pushar då talet till stacken.
+                    Operatorerna pushas till kön, likhetstecken tar ut två operander ur stacken och en
+                    operator ur kön och applicerar operatorn mellan dessa för att sedan pusha svaret tillbaka till stacken.
+                    Exit knappen avslutar programmet.""")
 
     def buttons(self, TEXT, COMMAND, WIDTH, HEIGHT, SIDE, COLOUR=None):
         if COLOUR == None:
@@ -79,13 +81,18 @@ class tkCalc():
         self.button.pack(side=SIDE)
 
     def label(self, TEXT, SIDE, ANCHOR):
-        self.label = Label(self.row, text=TEXT)
-        self.label.pack(side=SIDE, anchor=ANCHOR)
+        self.name = Label(self.row, text=TEXT)
+        self.name.pack(side=SIDE, anchor=ANCHOR)
 
-    def entry(self, WIDTH, COLOUR, SIDE, ANCHOR):
-        self.entry = Entry(self.row, textvariable=self.var, bg=COLOUR)
-        self.entry.config(width=WIDTH)
-        self.entry.pack(side=SIDE, anchor=ANCHOR)
+    def entryIn(self, WIDTH, COLOUR, SIDE, ANCHOR):
+        self.entryBox = Entry(self.row, textvariable=self.var, bg=COLOUR)
+        self.entryBox.config(width=WIDTH)
+        self.entryBox.pack(side=SIDE, anchor=ANCHOR)
+
+    def entryOut(self, WIDTH, COLOUR, SIDE, ANCHOR):
+        self.entryBox = Entry(self.row, textvariable=self.ans, bg=COLOUR)
+        self.entryBox.config(width=WIDTH)
+        self.entryBox.pack(side=SIDE, anchor=ANCHOR)
 
     def rowGeometry(self):
         self.row = Frame(self.window)
@@ -97,26 +104,32 @@ class tkCalc():
             self.row.pack()
 
     def mainloop(self):
-        #self.root.overrideredirect(False)
+        self.root.overrideredirect(False)
         self.window.mainloop()
 
+
 myCalc = tkCalc()
-myCalc.windowTopLevel(400, 133)
+myCalc.windowTopLevel(400, 160)
 myCalc.rowGeometry()
-myCalc.label("Inmatningsruta: ", LEFT, N)
-myCalc.entry(40, "yellow", LEFT, N)
+myCalc.label("Inmatning: ", LEFT, N)
+myCalc.entryIn(40, "yellow", LEFT, N)
 myCalc.rowPack(TOP)
 myCalc.rowGeometry()
-myCalc.buttons("+", myCalc.add, 10, 2, LEFT)
-myCalc.buttons("-", myCalc.sub, 10, 2, LEFT)
-myCalc.buttons("*", myCalc.mul, 10, 2, LEFT)
-myCalc.buttons("/", myCalc.div, 10, 2, LEFT)
-myCalc.buttons("^", myCalc.pwr, 10, 2, LEFT)
+myCalc.label("SvarsRuta:  ", LEFT, N)
+myCalc.entryOut(40, "yellow", LEFT, N)
 myCalc.rowPack()
 myCalc.rowGeometry()
-myCalc.buttons("Push", myCalc.push, 10, 2, LEFT)
-myCalc.buttons("=", myCalc.eq, 22, 2, LEFT)
-myCalc.buttons("Help", myCalc.printHelp, 10, 2, LEFT)
-myCalc.buttons("EXIT", myCalc.exit, 10, 2, LEFT, "red")
+myCalc.buttons("+", lambda: myCalc.buttonPress("+"), 10, 2, LEFT)
+myCalc.buttons("-", lambda: myCalc.buttonPress("-"), 10, 2, LEFT)
+myCalc.buttons("*", lambda: myCalc.buttonPress("*"), 10, 2, LEFT)
+myCalc.buttons("/", lambda: myCalc.buttonPress("/"), 10, 2, LEFT)
+myCalc.buttons("^", lambda: myCalc.buttonPress("^"), 10, 2, LEFT)
 myCalc.rowPack()
+myCalc.rowGeometry()
+myCalc.buttons("Push", lambda: myCalc.buttonPress("Push"), 10, 2, LEFT)
+myCalc.buttons("=", lambda: myCalc.buttonPress("="), 22, 2, LEFT)
+myCalc.buttons("Help", lambda: myCalc.buttonPress("Help"), 10, 2, LEFT)
+myCalc.buttons("EXIT", lambda: myCalc.buttonPress("EXIT"), 10, 2, LEFT, "red")
+myCalc.rowPack()
+myCalc.root.bind("<Return>", lambda x: myCalc.buttonPress("Push"))
 myCalc.mainloop()
